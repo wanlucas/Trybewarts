1fad006e721e170cdfde0ad3dcb28a648ba4ff08
wan lucas <wan.lucas.90@gmail.com>
2022-06-12 21:28:47 -0300

    :rocket: last adjusts

--- script.js
+++ script.js
@@ -21,17 +21,17 @@ function updateRemainingCharacters() {
   $('counter').innerText = 500 - this.value.length;
 }
 
-function fadeCard() {
-  const card = $('card');
+function fadeLetter() {
+  const letter = $('letter');
 
   $('main').style.animation = 'hidden 1s forwards';
-  card.style.visibility = 'visible';
-  card.style.animation = 'fade 1s forwards';
+  letter.style.visibility = 'visible';
+  letter.style.animation = 'fade 1s forwards';
 }
 
-function createCard(e) {
+function createLetter(e) {
   e.preventDefault();
-  fadeCard();
+  fadeLetter();
 
   [...new FormData(this)].forEach((data) => {
     const element = pth(`#form-data #${data[0]}`);
@@ -45,5 +45,5 @@ window.onload = () => {
   $('login').onclick = validateLogin;
   $('agreement').onchange = toggleButton;
   $('textarea').oninput = updateRemainingCharacters;
-  $('evaluation-form').onsubmit = createCard;
+  $('evaluation-form').onsubmit = createLetter;
 };
